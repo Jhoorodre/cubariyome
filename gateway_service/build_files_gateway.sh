d3a36ca87397ce0b95bdae6dd713676c4425c2e1
Jhoorodre <jhoorodre@gmail.com>
2025-06-23 15:31:24 -0300

    Remove backend gateway_service do projeto

--- gateway_service/build_files_gateway.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "--- Iniciando build_files_gateway.sh ---"
-echo "Instalando dependências Python..."
-pip install -r requirements.txt
-echo "Coletando arquivos estáticos Django..."
-python manage.py collectstatic --noinput
-echo "Aplicando migrações Django..."
-python manage.py migrate --noinput
-echo "--- build_files_gateway.sh concluído ---"
